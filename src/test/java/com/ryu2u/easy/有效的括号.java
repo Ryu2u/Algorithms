6eed5374aa9755382168acb0a87c30b8edbe4197
ryu2u <ryu2u@qq.com>
2023-06-11 22:02:48 +0800

    6.11

---
+++ src/test/java/com/ryu2u/easy/有效的括号.java
@@ -0,0 +1,86 @@
+package com.ryu2u.easy;
+
+import java.util.Stack;
+
+/**
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
+ * 有效字符串需满足：
+ * 左括号必须用相同类型的右括号闭合。
+ * 左括号必须以正确的顺序闭合。
+ * 每个右括号都有一个对应的相同类型的左括号。
+ * <p>
+ * 示例 1：
+ * 输入：s = "()"
+ * 输出：true
+ * <p>
+ * 示例 2：
+ * 输入：s = "()[]{}"
+ * 输出：true
+ * <p>
+ * 示例 3：
+ * 输入：s = "(]"
+ * 输出：false
+ *
+ * @author Ryu2u
+ * @Description:
+ * @date 2023/6/11 17:23
+ */
+public class 有效的括号 {
+
+    public static boolean isValid(String s) {
+        if (s == null || "".equals(s) || s.length() <= 1) {
+            return false;
+        }
+        char c = s.charAt(0);
+
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if ('(' == s.charAt(i) || '[' == s.charAt(i) || '{' == s.charAt(i)) {
+                stack.push(s.charAt(i));
+            } else {
+                // 说明是右括号，和栈顶的匹配
+                if (stack.isEmpty()){
+                    return false;
+                }
+                Character l = stack.pop();
+                Character r = s.charAt(i);
+                switch (l) {
+                    case '(': {
+                        if (r != ')') {
+                            return false;
+                        }
+                        break;
+                    }
+                    case '[': {
+                        if (r != ']') {
+                            return false;
+                        }
+                        break;
+                    }
+                    case '{': {
+                        if (r != '}') {
+                            return false;
+                        }
+                        break;
+                    }
+                    default: {
+                        return false;
+                    }
+                }
+            }
+        }
+        if (stack.isEmpty()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        String s = "()[]{}";
+        System.out.println(isValid(s));
+
+    }
+
+
+}
---
+++ src/test/java/com/ryu2u/medium/最长回文子串.java
@@ -0,0 +1,64 @@
+package com.ryu2u.medium;
+
+import java.util.Arrays;
+
+/**
+ * 给你一个字符串 s，找到 s 中最长的回文子串。
+ * 如果字符串的反序与原始字符串相同，则该字符串称为回文字符串。
+ *
+ * 示例 1：
+ * 输入：s = "babad"
+ * 输出："bab"
+ * 解释："aba" 同样是符合题意的答案。
+ *
+ * 示例 2：
+ * 输入：s = "cbbd"
+ * 输出："bb"
+ *
+ * @author Ryu2u
+ * @Description:
+ * @date 2023/6/11 17:55
+ */
+public class 最长回文子串 {
+    public static String longestPalindrome(String s) {
+        int len = s.length();
+        boolean [][] dp = new boolean[len][len];
+        for (int i = 0; i < len; i++) {
+            dp[i][i] = true;
+        }
+        int maxLen = 1;
+        int begin = 0;
+        for (int r = 2;r <= len ; r++){
+            for (int i = 0; i < len; i++) {
+                // j-i+1 = r
+                int j = r + i -1;
+                // 当前字符串长度
+                int l = j - i + 1;
+                if (j > len-1){
+                    //越界
+                    break;
+                }
+                if (s.charAt(i) != s.charAt(j)){
+                    dp[i][j] = false;
+                }else if(l <= 3){
+                    dp[i][j] = true;
+                }else{
+                    dp[i][j] = dp[i + 1][j-1];
+                }
+                if(dp[i][j] && l > maxLen){
+                    maxLen = l;
+                    begin = i;
+                }
+
+            }
+        }
+        return s.substring(begin,begin + maxLen);
+
+    }
+
+    public static void main(String[] args) {
+        String s = "babad";
+        String s1 = longestPalindrome(s);
+        System.out.println(s1);
+    }
+}

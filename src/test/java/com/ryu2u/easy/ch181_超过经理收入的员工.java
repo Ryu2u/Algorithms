64e2caf0eba3f6a5033ddd06366929504715323c
chenhaitao <cht@njxdht.com>
2023-07-11 16:57:15 +0800

    7.11

--- src/test/java/com/ryu2u/easy/ch181_超过经理收入的员工.java
+++ src/test/java/com/ryu2u/easy/ch181_超过经理收入的员工.java
@@ -1,10 +1,66 @@
 package com.ryu2u.easy;
 
 /**
+ *
+ Create table If Not Exists Employee (id int, name varchar(255), salary int, managerId int);
+ Truncate table Employee;
+ insert into Employee (id, name, salary, managerId) values ('1', 'Joe', '70000', '3');
+ insert into Employee (id, name, salary, managerId) values ('2', 'Henry', '80000', '4');
+ insert into Employee (id, name, salary, managerId) values ('3', 'Sam', '60000', null);
+ insert into Employee (id, name, salary, managerId) values ('4', 'Max', '90000', null);
+
+
+ 表：Employee
+
+ +-------------+---------+
+ | Column Name | Type    |
+ +-------------+---------+
+ | id          | int     |
+ | name        | varchar |
+ | salary      | int     |
+ | managerId   | int     |
+ +-------------+---------+
+ Id是该表的主键。
+ 该表的每一行都表示雇员的ID、姓名、工资和经理的ID。
+
+
+ 编写一个SQL查询来查找收入比经理高的员工。
+
+ 以 任意顺序 返回结果表。
+
+ 查询结果格式如下所示。
+
+
+
+ 示例 1:
+
+ 输入:
+ Employee 表:
+ +----+-------+--------+-----------+
+ | id | name  | salary | managerId |
+ +----+-------+--------+-----------+
+ | 1  | Joe   | 70000  | 3         |
+ | 2  | Henry | 80000  | 4         |
+ | 3  | Sam   | 60000  | Null      |
+ | 4  | Max   | 90000  | Null      |
+ +----+-------+--------+-----------+
+ 输出:
+ +----------+
+ | Employee |
+ +----------+
+ | Joe      |
+ +----------+
+ 解释: Joe 是唯一挣得比经理多的雇员。
+
  * @author Administrator
  * @Description:
  * @date 2023-06-30 13:28:51
  */
 public class ch181_超过经理收入的员工	{
+
+    /**
+     SELECT a.name as Employee FROM employee as a join employee as b on a.managerId = b.id and a.salary > b.salary
+
+     */
 
 }
--- src/test/java/com/ryu2u/easy/ch182_查找重复的电子邮箱.java
+++ src/test/java/com/ryu2u/easy/ch182_查找重复的电子邮箱.java
@@ -1,10 +1,56 @@
 package com.ryu2u.easy;
 
 /**
+  Create table If Not Exists Person (`id` int, email varchar(255));
+  Truncate table Person;
+  insert into Person (`id`, email) values ('1', 'a@b.com');
+  insert into Person (`id`, email) values ('2', 'c@d.com');
+  insert into Person (`id`, email) values ('3', 'a@b.com');
+ *
+ *
+ 表: Person
+
+ +-------------+---------+
+ | Column Name | Type    |
+ +-------------+---------+
+ | id          | int     |
+ | email       | varchar |
+ +-------------+---------+
+ id 是该表的主键列。
+ 此表的每一行都包含一封电子邮件。电子邮件不包含大写字母。
+ 编写一个 SQL 查询来报告所有重复的电子邮件。 请注意，可以保证电子邮件字段不为 NULL。
+ 以 任意顺序 返回结果表。
+ 查询结果格式如下例。
+
+
+
+ 示例 1:
+ 输入:
+ Person 表:
+ +----+---------+
+ | id | email   |
+ +----+---------+
+ | 1  | a@b.com |
+ | 2  | c@d.com |
+ | 3  | a@b.com |
+ +----+---------+
+ 输出:
+ +---------+
+ | Email   |
+ +---------+
+ | a@b.com |
+ +---------+
+ 解释: a@b.com 出现了两次。
  * @author Administrator
  * @Description:
  * @date 2023-06-30 13:28:51
  */
 public class ch182_查找重复的电子邮箱	{
+    /*
+    select a.email from (SELECT count(email) as c,email FROM `person` group by email ) as a where a.c > 1
+
+    // group by having
+    select email from person group by email having count(email) > 1
+     */
 
 }

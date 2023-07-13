4e07d3941517ecba3054b7720214166e0d20d562
chenhaitao <cht@njxdht.com>
2023-07-13 18:31:19 +0800

    7.13

---
+++ src/test/java/com/ryu2u/exercise/Test_20230713.java
@@ -0,0 +1,44 @@
+package com.ryu2u.exercise;
+
+import org.junit.Test;
+
+/**
+ * @author Administrator
+ * @Description:
+ * @date 2023/7/13 10:04
+ */
+public class Test_20230713 {
+
+    @Test
+    public void test(){
+        // ch70
+        System.out.println(climbStairs(45));
+        // ch136
+        int[] nums = {1,1,2,2,3};
+        System.out.println(singleNumber(nums));
+
+        // ch
+    }
+    public static int singleNumber(int[] nums) {
+        int ans = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            ans ^= nums[i];
+        }
+        return ans;
+    }
+
+    public static int climbStairs(int n) {
+        int pre = 0;
+        int cur = 0;
+        int res = 1;
+        for (int i = 1; i <= n; i++) {
+            pre = cur;
+            cur = res;
+            res = pre + cur;
+        }
+        return res;
+    }
+
+
+
+}

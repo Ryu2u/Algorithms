4d13d70badb27e18827612732c217c0c16c25508
chenhaitao <cht@njxdht.com>
2023-08-10 17:53:54 +0800

    8.10

---
+++ src/test/java/com/ryu2u/medium/ch343_剪绳子.java
@@ -0,0 +1,72 @@
+package com.ryu2u.medium;
+
+import org.junit.Test;
+
+/**
+ * 给你一根长度为 n 的绳子，请把绳子剪成整数长度的 m 段（m、n都是整数，n>1并且m>1），每段绳子的长度记为 k[0],k[1]...k[m-1] 。请问 k[0]*k[1]*...*k[m-1] 可能的最大乘积是多少？例如，当绳子的长度是8时，我们把它剪成长度分别为2、3、3的三段，此时得到的最大乘积是18。
+ * <p>
+ * 示例 1：
+ * 输入: 2
+ * 输出: 1
+ * 解释: 2 = 1 + 1, 1 × 1 = 1
+ * <p>
+ * 示例 2:
+ * 输入: 10
+ * 输出: 36
+ * 解释: 10 = 3 + 3 + 4, 3 × 3 × 4 = 36
+ * <p>
+ * 提示：
+ * 2 <= n <= 58
+ *
+ * @author Administrator
+ * @Description:
+ * @date 2023/8/10 17:28
+ */
+public class ch343_剪绳子 {
+    @Test
+    public void test() {
+        System.out.println(cuttingRope(10));
+    }
+
+
+    /**
+     * 数学法
+     *
+     * @param n
+     * @return
+     */
+    public int cuttingRope(int n) {
+        if (n <= 3) {
+            return n - 1;
+        }
+        int a = n / 3;
+        if (n % 3 == 0) {
+            return (int) Math.pow(3, a);
+        } else if (n % 3 == 1) {
+            return (int) Math.pow(3, a - 1) * 4;
+        } else {
+            return (int) Math.pow(3, a) * 2;
+        }
+
+    }
+
+    /**
+     * 动态规划
+     *
+     * @param n
+     * @return
+     */
+    public int cuttingRope2(int n) {
+        if (n <= 3) {
+            return n - 1;
+        }
+        int[] dp = new int[n + 1];
+        dp[2] = 1;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = Math.max(Math.max(2 * (i - 2), 2 * dp[i - 2]), Math.max(3 * (i - 3), 3 * dp[i - 3]));
+        }
+        return dp[n];
+    }
+
+
+}

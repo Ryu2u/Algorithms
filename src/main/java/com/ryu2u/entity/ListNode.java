99c4a818e7ce6ceb2b3e5424e8edb6fab13d688c
chenhaitao <cht@njxdht.com>
2023-09-04 19:10:40 +0800

    9.4

--- src/main/java/com/ryu2u/entity/ListNode.java
+++ src/main/java/com/ryu2u/entity/ListNode.java
@@ -3,6 +3,7 @@ package com.ryu2u.entity;
 
 /**
  * 链表类
+ *
  * @author Administrator
  * @Description:
  * @date 2023/6/13 13:27
@@ -10,21 +11,26 @@ package com.ryu2u.entity;
 public class ListNode {
     public int val;
     public ListNode next;
-    public ListNode(){}
-    public ListNode(int val){
+
+    public ListNode() {
+    }
+
+    public ListNode(int val) {
         this.val = val;
     }
-    public ListNode(int val, ListNode next){
+
+    public ListNode(int val, ListNode next) {
         this.val = val;
         this.next = next;
     }
-    public static void printList(ListNode head){
-        if(head == null){
+
+    public static void printList(ListNode head) {
+        if (head == null) {
             System.out.println("null");
             return;
         }
         System.out.print(head.val);
-        while (head.next != null){
+        while (head.next != null) {
             System.out.print(" --> ");
             System.out.print(head.next.val);
             head = head.next;
@@ -32,6 +38,18 @@ public class ListNode {
         System.out.println();
     }
 
+    public static ListNode arrayToListNode(int[] arr) {
+        if (arr.length <= 0) {
+            return null;
+        }
+        ListNode node = new ListNode(arr[0]);
+        ListNode newNode = node;
+        for (int i = 1; i < arr.length; i++) {
+            node.next = new ListNode(arr[i]);
+            node = node.next;
+        }
+        return newNode;
+    }
 
 
 }
---
+++ src/test/java/com/ryu2u/exercise/month9/Test_20230904.java
@@ -0,0 +1,131 @@
+package com.ryu2u.exercise.month9;
+
+import com.ryu2u.entity.ListNode;
+import com.ryu2u.entity.TreeNode;
+import org.junit.Test;
+
+import java.lang.reflect.Array;
+import java.util.*;
+
+/**
+ * @author Ryu2u
+ * @Description:
+ * @date 2023-09-04 17:22:54
+ */
+public class Test_20230904 {
+    @Test
+    public void test() {
+        // ch69
+        System.out.println(mySqrt(Integer.MAX_VALUE));
+        // ch18
+        // ch144
+        // ch92
+
+    }
+
+
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        ListNode node = new ListNode(0, head);
+        head = node;
+        for (int i = 0; i < left - 1; i++) {
+            node = node.next;
+        }
+        ListNode cur = node.next;
+        ListNode next = null;
+        for (int i = 0; i < right - left; i++) {
+            next = cur.next;
+            cur.next = next.next;
+            next.next = node.next;
+            node.next = next;
+        }
+        return head.next;
+    }
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if (root == null) {
+            return list;
+        }
+        Deque<TreeNode> stack = new LinkedList<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            root = stack.pop();
+            list.add(root.val);
+            if (root.right != null) {
+                stack.push(root.right);
+            }
+            if (root.left != null) {
+                stack.push(root.left);
+            }
+        }
+        return list;
+
+    }
+
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(nums);
+        int len = nums.length;
+        for (int i = 0; i < len - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            if ((long) nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target) {
+                break;
+            }
+            if ((long) nums[i] + nums[len - 3] + nums[len - 2] + nums[len - 1] < target) {
+                continue;
+            }
+            for (int j = i + 1; j < len - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+                if ((long) nums[i] + nums[j] + nums[j + 1] + nums[j + 2] > target) {
+                    break;
+                }
+                if ((long) nums[i] + nums[j] + nums[len - 2] + nums[len - 1] < target) {
+                    continue;
+                }
+                int l = j + 1;
+                int r = len - 1;
+                while (l < r) {
+                    long sum = nums[i] + nums[j] + nums[l] + nums[r];
+                    if (sum == target) {
+                        list.add(Arrays.asList(nums[i], nums[j], nums[l], nums[r]));
+                        while (l < r && nums[l] == nums[l + 1]) {
+                            l++;
+                        }
+                        while (l < r && nums[r] == nums[r - 1]) {
+                            r--;
+                        }
+                        l++;
+                        r--;
+                    } else if (sum > target) {
+                        r--;
+                    } else {
+                        l++;
+                    }
+                }
+            }
+        }
+        return list;
+    }
+
+    public int mySqrt(int x) {
+        int l = 0;
+        int r = x;
+        int ans = 0;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if ((long) mid * mid <= x) {
+                ans = mid;
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+
+}

e18fee8e98840919dabf8f589afa3818740a1566
ryu2u <ryu2u@qq.com>
2023-10-10 19:28:46 +0800

    10.10

---
+++ src/test/java/com/ryu2u/exercise/month10/Test_20231011.java
@@ -0,0 +1,15 @@
+package com.ryu2u.exercise.month10;
+import org.junit.Test;
+/**
+ * @author Ryu2u
+ * @Description:
+ * @date 2023-10-10 19:28:24
+ */
+public class Test_20231011	{
+    @Test
+    public void test(){
+        
+    }
+
+
+}
